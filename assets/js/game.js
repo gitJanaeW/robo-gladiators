e2504643fbed6ff53c6858dd6cf6f4fc0d5f3f02
janae.wel <janae.wel@gmail.com>
2022-06-06 15:27:49 -0400

    initialized one round of fight()

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,44 @@
 const playerName = window.prompt("What is your robo name?");
-console.log(playerName);
+let playerHealth = 100;
+let playerAttack = 10;
 
-function fight(){
-    window.alert("The fight has begun!");
+console.log(playerName, playerHealth, playerAttack);
 
-}
+var enemyName = "Roboto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// fight();
+console.log(enemyName, enemyHealth, enemyAttack);
+
+let roundNum = 1;
+
+var fight = function(){
+    window.alert("Welcome to Robo Galdiators!");
+    window.alert("Round " + roundNum + ": " + playerName + " vs. " + enemyName + "... BEGIN!");
+
+    // Player attack enemy + health (HP) log
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + "! " + enemyName + " now has " + enemyHealth + " HP remaining.");
+
+    // Check for enemy death:
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has signed off!");
+    }
+    else{
+        window.alert(enemyName + " is still standing with " + enemyHealth + " HP remaining.");
+    }
+
+    // Enemy attacks player + health (HP) log
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + "! " + playerName + " now has " + playerHealth + " HP remaining.");
+
+    if(playerHealth <= 0){
+        window.alert(playerName + " has signed off!");
+    }
+    else{
+        window.alert(playerName  + " is still standing with " + playerHealth + " HP remaining.");
+    }
+
+};
+
+fight();
