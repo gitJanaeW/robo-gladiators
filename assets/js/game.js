c16e650a8d634d0156cec52f5fc9016ffdde86cf
janae.wel <janae.wel@gmail.com>
2022-06-06 18:32:01 -0400

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,44 +1,75 @@
+// INITIALIZE PLAYER + ENEMIES
 const playerName = window.prompt("What is your robo name?");
 let playerHealth = 100;
 let playerAttack = 10;
+let playerMoney = 10;
 
-console.log(playerName, playerHealth, playerAttack);
-
-var enemyName = "Roboto";
+var enemyNames = ["Mr. Roboto", "Cindi Mayweather", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-console.log(enemyName, enemyHealth, enemyAttack);
+// SKIP CHOICE
+var skip = function(){
+    playerMoney = playerMoney - 10;
+}
 
+// FIGHT CHOICE
+// Round variable (for counting)
 let roundNum = 1;
 
-var fight = function(){
+var fight = function(enemyName){
     window.alert("Welcome to Robo Galdiators!");
-    window.alert("Round " + roundNum + ": " + playerName + " vs. " + enemyName + "... BEGIN!");
 
-    // Player attack enemy + health (HP) log
-    enemyHealth = enemyHealth - playerAttack;
-    console.log(playerName + " attacked " + enemyName + "! " + enemyName + " now has " + enemyHealth + " HP remaining.");
+    // Prompting the use to pick fight or skip
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    console.log(promptFight);
 
-    // Check for enemy death:
-    if (enemyHealth <= 0){
-        window.alert(enemyName + " has signed off!");
+    if(promptFight === "FIGHT" || promptFight === "fight"){
+        window.alert("Round " + roundNum + ": " + playerName + " vs. " + enemyName + "... BEGIN!");
+
+        // Player attack enemy + health (HP) log
+        enemyHealth = enemyHealth - playerAttack;
+        console.log(playerName + " attacked " + enemyName + "! " + enemyName + " now has " + enemyHealth + " HP remaining.");
+
+        // Check for enemy death:
+        if (enemyHealth <= 0){
+            window.alert(enemyName + " has signed off!");
+        }
+        else{
+            window.alert(enemyName + " is still standing with " + enemyHealth + " HP remaining.");
+        }
+
+        // Enemy attacks player + health (HP) log
+        playerHealth = playerHealth - enemyAttack;
+        console.log(enemyName + " attacked " + playerName + "! " + playerName + " now has " + playerHealth + " HP remaining.");
+
+        if(playerHealth <= 0){
+            window.alert(playerName + " has signed off!");
+        }
+        else{
+            window.alert(playerName  + " is still standing with " + playerHealth + " HP remaining.");
+        }
+    }
+    else if(promptFight === "SKIP" || promptFight === "skip"){
+        var confirmSkip = window.confirm("Skipping costs 2 MP (money points). Are you sure?");
+        if(confirmSkip){
+            playerMoney = playerMoney - 2;
+            window.alert(playerName + " has decided to skip this fight. " + playerMoney + "MP remaining.");
+        }
     }
     else{
-        window.alert(enemyName + " is still standing with " + enemyHealth + " HP remaining.");
+        fight();
     }
 
-    // Enemy attacks player + health (HP) log
-    playerHealth = playerHealth - enemyAttack;
-    console.log(enemyName + " attacked " + playerName + "! " + playerName + " now has " + playerHealth + " HP remaining.");
+    // Game States
+    // "WIN" - Player robot has defeated all enemy-robots
 
-    if(playerHealth <= 0){
-        window.alert(playerName + " has signed off!");
-    }
-    else{
-        window.alert(playerName  + " is still standing with " + playerHealth + " HP remaining.");
-    }
+    //    * Fight all enemy-robots
+
+    //    * Defeat each enemy-robot
+
+    // "LOSE" - Player robot's health is zero or less
 
 };
 
-fight();
+// fight();
