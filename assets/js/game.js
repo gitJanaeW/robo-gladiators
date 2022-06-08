1e1b01c48cd6600db5e1421a4ea40c863aa63123
janae.wel <janae.wel@gmail.com>
2022-06-08 14:31:34 -0400

    added shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,11 +10,11 @@ var enemyAttack = 12;
 
 var newFight = 1;
 
-// FIGHT CHOICE
 // Round variable (for counting)
 let roundNum = 1;
 
-var fight = function(enemyName){       
+// FIGHT
+var fight = function(enemyName){    
     while(enemyHealth > 0 && playerHealth > 0){
 
         // Prompting the use to pick fight or skip
@@ -25,9 +25,7 @@ var fight = function(enemyName){
         }else{
             var promptFight = window.prompt("FIGHT on, or SKIP this round?")
         }
-        
-        
-
+    
         // QUESTION: Why does the skip code have to be put above the fight code in order for the "break;" to work?
         if(promptFight === "SKIP" || promptFight === "skip"){
             var confirmSkip = window.confirm("Skipping costs 10MP (money points). Are you sure?");
@@ -88,13 +86,22 @@ const startGame = function(){
             enemyHealth = 50;
             newFight = 1;
             fight(pickedEnemyName);
+
+            // Why do we need to check player health if the parent if statement already checks if the player is alive?
+            if(playerHealth > 0 && i < enemyNames.length - 1){
+                var storeConfirm = window.confirm("The fight is over. Visit the store before the next round?");
+
+                if(storeConfirm){
+                    shop();
+                }
+            }
         }else{
-            window.alert("You have lost your robot in battle! GAME OVER.")
+            window.alert("You have lost your robot in battle! GAME OVER.");
             break;
         }
     }
-    startGame();
-}
+    endGame();
+};
 
 // End pf the entire game
 const endGame = function(){
@@ -114,8 +121,47 @@ const endGame = function(){
     else{
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
-}
+};
 
+// Shop function
+var shop = function(){
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', UPGRADE', 'LEAVE' to make a choice."
+    );
 
-endGame();
+    switch (shopOptionPrompt){
+        // Because there's no break in "refill", it falls through to the conditions that are applied to "REFILL". You can add as many conditions as you want
+        case "refill":
+        case "REFILL":
+            if(playerMoney < 7){
+                window.alert("You do not have enough money to purchase this item. Player money: " + playerMoney + ".");
+            }else{
+                window.alert("Refilling player's health by 20 for 7MP (money points).");
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney - 7;
+            }
+            break;
+        case "upgrade":
+        case "UPGRADE":
+            if(playerMoney < 7){
+                window.alert("You do not have enough money to purchase this item. Player money: " + playerMoney + ".");
+            }else{
+                window.alert("Upgrading player's attack by 6 for 7MP (money points).");
+            playerAttack = playerAttack + 6;
+            playerMoney = playerMoney - 7;
+            }
+            break;
+        case "leave":
+        case "LEAVE":
+            window.alert("Leaving the store...");
+            break;
+        default:
+            window.alert("You did not pick a valid option. Please try again.");
+            shop();
+            break;
+    }
+};
+
+// Start first game when page loads
+startGame();
 
